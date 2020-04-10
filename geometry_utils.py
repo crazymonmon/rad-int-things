668a554b539785680f756fb652785179d4e013a0
Swarnabha Sen <swarnabha@indeed.com>
2020-04-11 02:30:49 +0530

    Calculate values for 360 degrees

--- geometry_utils.py
+++ geometry_utils.py
@@ -4,10 +4,7 @@ import math
 from point import Point
 
 
-def generate_list_of_points(theta: int, radius: int, centre: Point):
-    if theta > 180:
-        raise Exception("Theta greater than 180.")
-
+def generate_list_of_points(theta: float, radius: int, centre: Point):
     list_of_points = []
 
     target_point = get_point_at_distance(theta, radius, centre)
@@ -15,29 +12,48 @@ def generate_list_of_points(theta: int, radius: int, centre: Point):
     dx = target_point.x - centre.x
     dy = target_point.y - centre.y
 
+    # TODO: Simplify this if condition
     if theta < 45:
         for x in range(centre.x, target_point.x + 1):
             # y  = y1 + (x - x1)/m
             y = centre.y + dy * (x - centre.x)/dx
             list_of_points.append(Point(x, y))
     elif theta < 90:
-        logging.debug("centre: %s target: %s" % (centre, target_point))
         for y in range(centre.y, target_point.y + 1):
             # x  = x1 + m * (y - y1)
             x = centre.x + dx * (y - centre.y)/dy
             list_of_points.append(Point(x, y))
     elif theta < 135:
-        logging.debug("centre: %s target: %s" % (centre, target_point))
         for y in range(centre.y, target_point.y + 1):
             # x  = x1 + m * (y - y1)
             x = centre.x + dx * (y - centre.y) / dy
             list_of_points.append(Point(x, y))
-    elif theta <= 180:
+    elif theta < 180:
         for x in range(target_point.x, centre.x + 1):
             # y  = y1 + (x - x1)/m
             y = centre.y + dy * (x - centre.x)/dx
             list_of_points.append(Point(x, y))
-    logging.debug("theta: %.2f, Number of points: %d" % (theta, len(list_of_points)))
+    elif theta < 225:
+        for x in range(target_point.x, centre.x + 1):
+            # y  = y1 + (x - x1)/m
+            y = centre.y + dy * (x - centre.x)/dx
+            list_of_points.append(Point(x, y))
+    elif theta < 270:
+        for y in range(target_point.y, centre.y + 1):
+            # x  = x1 + m * (y - y1)
+            x = centre.x + dx * (y - centre.y)/dy
+            list_of_points.append(Point(x, y))
+    elif theta < 315:
+        for y in range(target_point.y, centre.y + 1):
+            # x  = x1 + m * (y - y1)
+            x = centre.x + dx * (y - centre.y) / dy
+            list_of_points.append(Point(x, y))
+    elif theta < 360:
+        for x in range(target_point.x, centre.x + 1):
+            # y  = y1 + (x - x1)/m
+            y = centre.y + dy * (x - centre.x)/dx
+            list_of_points.append(Point(x, y))
+    # logging.debug("theta: %.2f, Number of points: %d" % (theta, len(list_of_points)))
 
     return list_of_points
 
@@ -55,15 +71,23 @@ def get_point_at_distance(theta: float, radius: int, origin=Point(0, 0)):
     elif theta == 180:
         target.x = origin.x + (-1 * radius)
         target.y = origin.y
-    elif theta > 180:
-        raise Exception("Value of theta > 180.")
-    else:
+    elif theta == 270:
+        target.x = origin.x
+        target.y = origin.y - radius
+    elif theta < 180:
         m = math.tan(math.radians(theta))
         dx = radius/(math.sqrt(1+(m*m)))
         dy = m * dx
 
         target.x = int((origin.x + dx) if theta < 90 else (origin.x - dx))
         target.y = int((origin.y + dy) if theta < 90 else (origin.y - dy))
+    else:
+        m = math.tan(math.radians(theta))
+        dx = radius/(math.sqrt(1+(m*m)))
+        dy = m * dx
+
+        target.x = int((origin.x - dx) if theta < 270 else (origin.x + dx))
+        target.y = int((origin.y - dy) if theta < 270 else (origin.y + dy))
     # logging.debug("origin: %s, theta: %.2f, radius: %.1f, target: %s" % (origin, theta, radius, target))
 
     return target
--- main.py
+++ main.py
@@ -1,43 +1,53 @@
 import logging
 
-import cv2
-
 from geometry_utils import generate_list_of_points
 from image_utils import ImageHandler
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
-    logging.info("Running main")
+    logging.debug("Running main")
 
     # Get image data
-    img_filepath = "./img.png"
+    img_filepath = "../img.png"
     img_h = ImageHandler(img_filepath)
 
     # Identify centre and radius of circle
     centre = img_h.centre
     radius = int(img_h.width / 2)
 
+    # Uncomment to debug
+    # import cv2
+    # from geometry_utils import get_point_at_distance
+    # for theta in range(0, 360):
+    #     point = get_point_at_distance(theta, radius, centre)
+    #     img_h.img[point.x - 1, point.y - 1] = 0
+    #
+    #     print(theta, point)
+    #     cv2.imshow("test", img_h.img)
+    #     cv2.waitKey(10)
+
     # Increment value of theta
-    for theta in range(0, 180+1):
-        # for each value of theta identify the the points and sum the intensity
+    for theta in range(0, 360, 1):
+        # For each value of theta identify the the points
         list_of_points = generate_list_of_points(theta, radius, centre)
 
-        for point in list_of_points:
-            img_h.img[point.x - 1, point.y - 1] = 0
+        # Uncomment to debug
+        # for point in list_of_points:
+        #     img_h.img[point.x - 1, point.y - 1] = 0
+        # if theta % 45 == 0:
+        #     cv2.imshow("test", img_h.img)
+        #     cv2.waitKey(1000)
 
-        if theta == 45 or theta == 90 or theta == 135:
-            cv2.imshow("test", img_h.img)
-            cv2.waitKey(0)
-
+        # Add the intensity of the pixels
         integral = img_h.add_intensity_of_points(list_of_points)
 
         # store value of theta and value of pixel
-        # print("%.2f, %d" % (theta, integral))
-    cv2.imshow("test", img_h.img)
-    cv2.waitKey(0)
+        print("%.2f, %d" % (theta, integral))
 
-
+    # Uncomment to debug
+    # cv2.imshow("test", img_h.img)
+    # cv2.waitKey(0)
 
 
 if __name__ == "__main__":
